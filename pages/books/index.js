cefd00a6961704ddde784bc34fc4d12d1c390afd
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-09-09 12:04:11 -0500

    wrote a sick ticket

--- pages/books/index.js
+++ pages/books/index.js
@@ -57,10 +57,7 @@ function BookList() {
               <BookCard
                 id={book.id}
                 imageUrl={book.image_url}
-                // publisher={book.publisher}
                 title={book.title}
-                // price={book.price}
-                // description={book.description}
                 onUpdate={displayBooks} // Pass 'displayBooks' as a prop for updating
               />
             </section>
