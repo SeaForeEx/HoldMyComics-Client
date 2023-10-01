41f3d8af1b0adacfab3a9a4c42d45b68567396b1
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-09-30 20:03:20 -0500

    BookList displays titles in a table, not cards

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react-hooks/exhaustive-deps */ // Disable specific ESLint rule for the next line
 import React, { useState, useEffect } from 'react'; // Importing React, useState, and useEffect from React
 import { useRouter } from 'next/router';
-import BookCard from '../../components/cards/BookCard'; // Importing the 'BookCard' component
 import { getAllBooks } from '../../utils/data/bookData'; // Importing function to fetch all books
 
 // React functional component for displaying a list of books
@@ -54,29 +54,101 @@ function BookList() {
     document.title = 'COMIC BOOKS!';
   }, []);
 
+  const handleTitleClick = (bookId) => {
+    // Use router.push to navigate to the book's page
+    router.push(`/books/${bookId}`);
+  };
+
+  // Calculate the number of books per column
+  const booksPerColumn = Math.ceil(books.length / 3);
+
+  // books.length is the total number of books in your list.
+
+  // Math.ceil() is a JavaScript function that rounds a number up to the nearest integer. It's used here to ensure that each column has approximately the same number of books. If the total number of books is not evenly divisible by 3, this will round up to the nearest whole number.
+
+  // Split the books into three arrays
+  const firstColumnBooks = books.slice(0, booksPerColumn);
+  const secondColumnBooks = books.slice(booksPerColumn, 2 * booksPerColumn);
+  const thirdColumnBooks = books.slice(2 * booksPerColumn);
+
+  // books.slice(start, end) is a JavaScript method that returns a new array containing a portion of the original books array. It extracts elements from the start index up to (but not including) the end index.
+
+  // firstColumnBooks contains the first set of books, starting from the beginning (index 0) up to booksPerColumn.
+
+  // secondColumnBooks contains the second set of books, starting from booksPerColumn and ending at 2 * booksPerColumn.
+
+  // thirdColumnBooks contains the third set of books, starting from 2 * booksPerColumn to the end of the original array.
+
   // JSX to render the list of books
   return (
     <article className="text-center my-4" id="books">
       <h1 style={{ marginTop: '40px' }}>Comics Released The Week of</h1>
       <h1 style={{ marginBottom: '40px' }}>{pickedMondayDate}</h1>
 
-      <div className="d-flex flex-wrap">
-        {/* Loop through 'books' array and render each book */}
-        {books.map((book) => (
-          <section
-            key={`book--${book.id}`} // Using book's ID as the key
-            className="book"
-            style={{ margin: '0px' }}
-          >
-            {/* Render the 'BookCard' component with book details */}
-            <BookCard
-              id={book.id}
-              imageUrl={book.image_url}
-              title={book.title}
-              onUpdate={displayBooks} // Pass 'displayBooks' as a prop for updating
-            />
-          </section>
-        ))}
+      {/* Create a table structure */}
+      <div className="d-flex justify-content-between">
+        {/* First Column */}
+        <table className="custom-table">
+          <tbody>
+            {firstColumnBooks.map((book) => (
+              <tr key={`book--${book.id}`}>
+                <td>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleTitleClick(book.id);
+                    }}
+                  >
+                    {book.title}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Second Column */}
+        <table className="custom-table">
+          <tbody>
+            {secondColumnBooks.map((book) => (
+              <tr key={`book--${book.id}`}>
+                <td>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleTitleClick(book.id);
+                    }}
+                  >
+                    {book.title}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Third Column */}
+        <table className="custom-table">
+          <tbody>
+            {thirdColumnBooks.map((book) => (
+              <tr key={`book--${book.id}`}>
+                <td>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleTitleClick(book.id);
+                    }}
+                  >
+                    {book.title}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
     </article>
